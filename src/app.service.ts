181e68009ee34d95e6a7cc6e0cf5225760b81ac8
renatogastonruizgithub <renatogastonruiz.00@gmail.com>
2025-04-05 14:26:02 -0300

    1er test

---
+++ src/app.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import * as ExcelJS from 'exceljs';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'Hello World!';
+  }
+
+  async overwriteExcel/* (filePath: string): Promise<void> */() {
+
+    let filePath ="./remito/Book1.xlsx"
+    const workbook = new ExcelJS.Workbook();
+    await workbook.xlsx.readFile(filePath);
+
+    const sheet = workbook.getWorksheet(1); // Obtén la primera hoja
+    sheet.getCell('A1').value = 'Nueva información fdfdffdf'; // Sobreescribe una celda
+
+    await workbook.xlsx.writeFile(filePath); // Guarda los cambios
+    console.log('Archivo Excel sobrescrito exitosamente');
+  }
+
+}
